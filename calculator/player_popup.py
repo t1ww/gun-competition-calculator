383d72f1ac3f7ca39beebabbe89bcbfc014c1b45
t1ww <nongtews@gmail.com>
2024-08-06 09:51:03 +0700

    still fixing

--- calculator/player_popup.py
+++ calculator/player_popup.py
@@ -1,6 +1,7 @@
 # player_popup.py
 import tkinter as tk
 from classes import Player
+import global_var
 
 def add_new_player(root, players):
     def submit_player_info():
@@ -14,9 +15,11 @@ def add_new_player(root, players):
             # .5
             # .75
             # 1
+            handicap = int(handicap) # change this to drop down
             new_player = Player(root, players, name=name, handicap=handicap)
             players.append(new_player)
-            new_player.add_score_entry(root)  # Optionally add a score entry for the new player
+            for i in range(global_var.rounds):
+                new_player.add_score_entry(root)
             popup.destroy()
         except ValueError:
             label_error.config(text="Please enter valid numbers for rounds and handicap.")
@@ -30,24 +33,24 @@ def add_new_player(root, players):
     ### INPUTS
 
     # NAME 
-    label_name = tk.Label(popup, text="Name:")
+    label_name = tk.Label(popup, text="Name: ")
     label_name.grid(row=0, column=0, padx=pad_x, pady=pad_y)
     entry_name = tk.Entry(popup)
     entry_name.grid(row=0, column=1, padx=pad_x, pady=pad_y)
     
     # HANDICAP
-    label_handicap = tk.Label(popup, text="Handicap:")
+    label_handicap = tk.Label(popup, text="Handicap: ")
     label_handicap.grid(row=1, column=0, padx=pad_x, pady=pad_y)
     handicap_options = ["0", ".25", ".5", ".75", "1"]
     handicap_var = tk.StringVar()
-    combobox_handicap = tk.Combobox(popup, textvariable=handicap_var, values=handicap_options, state="readonly")
-    combobox_handicap.grid(row=1, column=1, padx=pad_x, pady=pad_y)
-    combobox_handicap.current(0)  # Set the default value to "0"
+    handicap_var.set("0")
+    combobox_handicap = tk.OptionMenu(popup, handicap_var, *handicap_options)
+    combobox_handicap.pack()
     
     # SUBMIT BUTTON
     button_submit = tk.Button(popup, text="Submit", command=submit_player_info)
-    button_submit.grid(row=3, column=0, columnspan=2, padx=pad_x, pady=pad_y)
+    button_submit.grid(row=2, column=0, columnspan=2, padx=pad_x, pady=pad_y)
     
     # error
     label_error = tk.Label(popup, text="", fg="red")
-    label_error.grid(row=4, column=0, columnspan=2, padx=pad_x, pady=pad_y)
+    label_error.grid(row=3, column=0, columnspan=2, padx=pad_x, pady=pad_y)
